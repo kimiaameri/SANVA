a88e4a014ed32e52e10fc04f48db8afe684b5198
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-05 16:50:45 -0600

    Create picard.sh

---
+++ picard.sh
@@ -0,0 +1,13 @@
+module load picard/2.18
+
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_HSS12071M1_pool_S24.sorted.bam O=$WORK/SAEVA-outputs/picard/HSS12071M1.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/HSS12071M1.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman94232M1_S14.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman94232M1_S14.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman94232M1_S14.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman99382M1_S15.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman99382M1_S15.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman99382M1_S15.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman99624D1_S16.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman99624D1_S16.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman99624D1_S16.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman62129B1_S17.sorted.bam O=$WORK/SAEVA_outputs/picard/Silverman62129B1_S17.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman62129B1_S17.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman60383B1_S18.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman60383B1_S18.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman60383B1_S18.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman93954A1_S19.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman93954A1_S19.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman93954A1_S19.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman99624A1_S20.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman99624A1_S20.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman99624A1_S20.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman37712B1_S21.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman37712B1_S21.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman37712B1_S21.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman99382B1_S22.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman99382B1_S22.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman99382B1_S22.picard.log;
+picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/aln_Silverman79414A1_S23.sorted.bam O=$WORK/SAEVA-outputs/picard/Silverman79414A1_S23.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/Silverman79414A1_S23.picard.log;
