ae92c4ea8af3d1d4b44dce8e9e60fa4cf66d5c11
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-15 17:09:36 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,3 +33,6 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 conda install –c bioconda biopython
 conda install –c bioconda bedops
+
+###########################
+echo `$WORK/SAEVA-softwares/freebayes/bin/freebayes --version`
