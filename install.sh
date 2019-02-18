498c4ca5072e685a1758156672abdc0d3f99327a
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 15:02:07 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ conda install –c bioconda trimmomatic
 conda install –c bioconda biopython
 conda install –c bioconda bedops
 ####   Download BWA       #####
+cd $WORK/SAEVA-softwares
+
 wget https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.17.tar.bz2
 tar -xvjf bwa-0.7.17.tar.bz2
 rm bwa-0.7.17.tar.bz2
