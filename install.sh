9de3d64aa92113bbd652d5d1f69f2b334020455e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 17:23:01 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ conda install –c bioconda trimmomatic
 conda install –c bioconda biopython
 conda install –c bioconda bedops
 conda install -c bioconda samtools/1.6
-conda install -c bioconda picard/2.18
+conda install -c bioconda picard
 ####   Download BWA       #####
 cd $WORK/SAEVA-softwares
 
