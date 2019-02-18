0e2dd854cc93d612fb3d047a6aa7859345f74312
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 15:16:45 -0600

    Create pythonBam.py

---
+++ pythonBam.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+
+if len(sys.argv) < 3:
+    sys.stderr.write('No Input CSV file and miniconda path\n')
+    sys.exit(0)
+    
+inputFile = sys.argv[1]
+minicondaBin = sys.argv[2]
+outputFile = "trimmomatic.sh"
+with open(outputFile,'w') as outFile:
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            outFile.write(f'{minicondaBin}samtools view -bt $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SAEVA-outputs/samfiles/{row[0]}.sam >$WORK/SAEVA-outputs/bamfiles/{row[0]}.bam\n')
+            outFile.write(f'{minicondaBin}samtools flagstat $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam > $WORK/SAEVA-outputs/flagsam/{row[0]}.flagstat.log\n')
+            outFile.write(f'{minicondaBin}samtools sort $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam -O bam -o $WORK/SAEVA-outputs/flagsam/{row[0]}.sorted.bam\n')
+
+ 
