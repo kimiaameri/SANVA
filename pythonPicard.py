06b2eb5e69b2d59c7a50fcca474f0b6c40dc208d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 17:14:03 -0600

    Update pythonPicard.py

--- pythonPicard.py
+++ pythonPicard.py
@@ -1,15 +1,15 @@
 import csv
 import sys
-
-if len(sys.argv) < 2:
-    sys.stderr.write('No Input CSV file \n')
+if len(sys.argv) < 3:
+    sys.stderr.write('No Input CSV file and miniconda path\n')
     sys.exit(0)
     
-
 inputFile = sys.argv[1]
+minicondaBin = sys.argv[2]
+
 outputFile = "picard.sh"
 with open(outputFile,'w') as outFile:
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.bam O=$WORK/SAEVA-outputs/picard/{row[0]}.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/{row[0]}.picard.log\n')
+            outFile.write(f'{minicondaBin}picard MarkDuplicates I=$WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.bam O=$WORK/SAEVA-outputs/picard/{row[0]}.picard.bam M=$WORK/SAEVA-outputs/picard/picardlog/{row[0]}.picard.log\n')
