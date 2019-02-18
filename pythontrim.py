e1fe03af23d4f5c29b51188c086acfe419f53ea6
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 14:21:08 -0600

    Update pythontrim.py

--- pythontrim.py
+++ pythontrim.py
@@ -6,9 +6,11 @@ if len(sys.argv) < 2:
     sys.exit(0)
     
 inputFile = sys.argv[1]
-with open(inputFile) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-            print(f'\t trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[1]} {row[2]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq')
-
+outputFile = "trimmomatic.sh"
+with open(outputFile,'w') as outFile:
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            outfile.write('\t trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq\n')
+            
  
