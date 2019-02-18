a56dbbd10892d9214dccd65a10a82cdd33fe0c91
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 14:15:29 -0600

    Update pythontrim.py

--- pythontrim.py
+++ pythontrim.py
@@ -1,16 +1,14 @@
 import csv
-f = open("InputFiles.csv",'r')
-csv_reader = csv.reader(csv_file, delimiter=',')
+import sys
+
+if len(sys.argv) < 2:
+    sys.stderr.write('No Input CSV file\n')
+    sys.exit(0)
     
-with open('InputFiles.csv') as csv_file:
+inputFile = sys.argv[1]
+with open(inputFile) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
             print(f'\t trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[1]} {row[2]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq')
 
-                  
-with open ("InputFiles.csv", "r") as myfile:
-    data = myfile.read()
-    for line in data:
-        line.rstrip()
-
-print data
+ 
