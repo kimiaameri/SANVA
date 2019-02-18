fa76bffc249d72ef9bfeb9559815fb39890c2bf0
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 14:29:15 -0600

    Update pythontrim.py

--- pythontrim.py
+++ pythontrim.py
@@ -1,16 +1,17 @@
 import csv
 import sys
 
-if len(sys.argv) < 2:
-    sys.stderr.write('No Input CSV file\n')
+if len(sys.argv) < 3:
+    sys.stderr.write('No Input CSV file and miniconda path\n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
+minicondaBin = sys.argv[2]
 outputFile = "trimmomatic.sh"
 with open(outputFile,'w') as outFile:
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'\t trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq\n')
+            outFile.write(f'{minicondaBin/}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq\n')
             
  
