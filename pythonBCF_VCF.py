cde62d7ee5c2a9ec4404850c27b0d376b3787511
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-18 18:35:37 -0600

    Create pythonBCF_VCF.py

---
+++ pythonBCF_VCF.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+
+if len(sys.argv) < 3:
+    sys.stderr.write('No Input CSV file and miniconda path\n')
+    sys.exit(0)
+    
+inputFile = sys.argv[1]
+minicondaBin = sys.argv[2]
+
+outputFile = "BCF_VCF.sh"
+with open(outputFile,'w') as outFile:
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            outFile.write(f'$WORK/SAEVA-softwares/freebayes/vcflib/bin/vcffilter -f "QUAL > 5000" $WORK/SAEVA-outputs/freebayesoutput/{row[0]}.vcf >$WORK/SAEVA-outputs/vcffilterq5000/{row[0]}.vcf\n')
+            outFile.write(f'WORK/SAEVA-softwares/freebayes/vcflib/bin/vcffilter -f "DP > 250" $WORK/SAEVA-outputs/vcffilterq5000/{row[0]}.vcf > $WORK/SAEVA-outputs/vcffilterq5000-dp250/{row[0]}.vcf\n')
+            outFile.write(f'{minicondaBin}bcftools view -Ob $WORK/SAEVA-outputs/vcffilterq5000-dp250/{row[0]}.vcf > $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')
+            outFile.write(f'{minicondaBin}bcftools index $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')
