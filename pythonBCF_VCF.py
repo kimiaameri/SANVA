8c900f3133d4ae15c4781af1923bcadd91e6a727
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-22 17:02:05 -0600

    Update pythonBCF_VCF.py

--- pythonBCF_VCF.py
+++ pythonBCF_VCF.py
@@ -1,19 +1,20 @@
 import csv
 import sys
 
-if len(sys.argv) < 4:
-    sys.stderr.write('No Input CSV file and miniconda path  and depth \n')
+if len(sys.argv) < 5:
+    sys.stderr.write('No Input CSV file and miniconda path  and qulaity and depth \n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
-depth= sys.argv[3]
+qulaity = sys.argv[3]
+depth= sys.argv[4]
 outputFile = "BCF_VCF.sh"
 with open(outputFile,'w') as outFile:
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'$WORK/SAEVA-softwares/freebayes/vcflib/bin/vcffilter -f "QUAL > 5000" $WORK/SAEVA-outputs/freebayesoutput/{row[0]}.vcf >$WORK/SAEVA-outputs/vcffilterq5000/{row[0]}.vcf\n')
-            outFile.write(f'WORK/SAEVA-softwares/freebayes/vcflib/bin/vcffilter -f "DP > {depth}" $WORK/SAEVA-outputs/vcffilterq5000/{row[0]}.vcf > $WORK/SAEVA-outputs/vcffilterq5000-dp250/{row[0]}.vcf\n')
-            outFile.write(f'{minicondaBin}bcftools view -Ob $WORK/SAEVA-outputs/vcffilterq5000-dp250/{row[0]}.vcf > $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')
+            outFile.write(f'$WORK/SAEVA-softwares/freebayes/vcflib/bin/vcffilter -f "QUAL >{qulaity}" $WORK/SAEVA-outputs/freebayesoutput/{row[0]}.vcf >$WORK/SAEVA-outputs/vcffilterq/{row[0]}.vcf\n')
+            outFile.write(f'WORK/SAEVA-softwares/freebayes/vcflib/bin/vcffilter -f "DP > {depth}" $WORK/SAEVA-outputs/vcffilterq/{row[0]}.vcf > $WORK/SAEVA-outputs/vcffilterq-dp/{row[0]}.vcf\n')
+            outFile.write(f'{minicondaBin}bcftools view -Ob $WORK/SAEVA-outputs/vcffilterq-dp/{row[0]}.vcf > $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')
             outFile.write(f'{minicondaBin}bcftools index $WORK/SAEVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')
