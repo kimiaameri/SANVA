314d8eabbe11bb9490bd34ecb69be0cda27b3e94
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-25 15:35:02 -0600

    Update SAEVA.sh

--- SAEVA.sh
+++ SAEVA.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=SAVEA.%J.err
 #SBATCH --output=SAVEA.%J.out
 ######## Trimmomatic #########
-export MINICONDA_HOME="~/miniconda2/bin/"
+export MINICONDA_HOME="~/miniconda3/bin/"
 
 cd $WORK
 mkdir SAEVA-outputs
@@ -16,14 +16,14 @@ cd trimmomatic
 mkdir trimlog
 
 cd $WORK/SAEVA
-python pythonTrimmomatic.py ../InputFiles.csv $MINICONDA_HOME
+python3 pythonTrimmomatic.py ../InputFiles.csv $MINICONDA_HOME
 
 sh trimmomatic.sh
 ########### BWA  ###############
 cd $WORK/SAEVA-outputs
 mkdir samfiles
 cd $WORK/SAEVA
-python pythonBwa.py ../InputFiles.csv 
+python3 pythonBwa.py ../InputFiles.csv 
 sh bwa.sh
 ###########  BAM ##################
 cd $WORK/SAEVA-outputs
@@ -33,7 +33,7 @@ mkdir sortsam
 mkdir depth
 cd $WORK/SAEVA
 
-python pythonBam.py ../InputFiles.csv $MINICONDA_HOME
+python3 pythonBam.py ../InputFiles.csv $MINICONDA_HOME
 sh bam.sh
 sh findDepth.sh
 
@@ -44,7 +44,7 @@ mkdir picard
 cd picard
 mkdir picardlog
 cd $WORK/SAVEA
-python pythonPicard.py ../InputFiles.csv $MINICONDA_HOME
+python3 pythonPicard.py ../InputFiles.csv $MINICONDA_HOME
 
 sh picard.sh
 ###########  Freebayes ##################
@@ -52,7 +52,7 @@ cd $WORK/SAEVA-outputs
 mkdir freebayesoutput
 
 cd $WORK/SAEVA
-python pythonFreebayes.py ../InputFiles.csv 
+python3 pythonFreebayes.py ../InputFiles.csv 
 
 sh freebayes.sh
 
@@ -61,7 +61,7 @@ sh freebayes.sh
 Rscript depth.R $WORK/SAEVA-outputs/depth $WORK/SAEVA-outputs/freebayesoutput depth.txt quality.txt summary.csv
 DEPTH=`cat depth.txt`
 QUALITY=`cat quality.txt`
-python pythonFinddepth.py ../InputFiles.csv $MINICONDA_HOME $QUALITY $DEPTH
+python3 pythonFinddepth.py ../InputFiles.csv $MINICONDA_HOME $QUALITY $DEPTH
 
 
 ###########  VCF-BCF ##################
@@ -71,7 +71,7 @@ mkdir bcfoutput
 mkdir vcffilterq-dp
 mkdir vcfbed
 cd $WORK/SAEVA
-python pythonBCF_VCF.py ../InputFiles.csv $MINICONDA_HOME $QUALITY $DEPTH
+python3 pythonBCF_VCF.py ../InputFiles.csv $MINICONDA_HOME $QUALITY $DEPTH
 sh BCF-VCF.sh
 ###########  snpEFF ##################
 cd $WORK/SAEVA-outputs
@@ -80,5 +80,5 @@ cd snpEff-outputs
 mkdir snpEff-summary
 mkdir snpEff-gene
 cd $WORK/SAEVA
-python pythonSnpEff.py ../InputFiles.csv $MINICONDA_HOME
+python3 pythonSnpEff.py ../InputFiles.csv $MINICONDA_HOME
 sh snpEff.sh
