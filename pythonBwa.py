d8240dfa90880994c9a01eef356f2651f29baa99
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-25 15:38:16 -0600

    Update pythonBwa.py

--- pythonBwa.py
+++ pythonBwa.py
@@ -9,7 +9,10 @@ if len(sys.argv) < 2:
 inputFile = sys.argv[1]
 outputFile = "BWA.sh"
 with open(outputFile,'w') as outFile:
+    count =0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'$WORK/SAEVA-softwares/bwa-0.7.17/bwa mem $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R2.paired.fq >$WORK/SAEVA-outputs/samfiles/{row[0]}.sam\n')
+            if count !=0:
+                outFile.write(f'$WORK/SAEVA-softwares/bwa-0.7.17/bwa mem $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R2.paired.fq >$WORK/SAEVA-outputs/samfiles/{row[0]}.sam\n')
+            count =count+1
