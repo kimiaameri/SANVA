5556a71cf1c780ca882ece06aa8df8e61e4a4777
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-25 15:49:59 -0600

    Update pythonSnpEff.py

--- pythonSnpEff.py
+++ pythonSnpEff.py
@@ -10,18 +10,23 @@ minicondaBin = sys.argv[2]
 outputFile = "SnpEff.sh"
 csv_reader = csv.reader(csv_file, delimiter=',')
 length = len(list(csv.reader(open('csv_reader'))))
+for row in csv_reader:
+    length= csv_reader.nrows
+count(csv_reader[2].count(High)
+count(csv_reader.index == )
+highIndex =  [index for index, value in enumerate(csv_reader[2]) if value == "high"]
+lowIndexx =  [index for index, value in enumerate(csv_reader[2]) if value == "low"]
 
-sum(csv_reader.index == 17)
-sum(csv_reader.index == 17)
-sum(csv_reader.index == 17)
-
+samples = csv_reader[1]
+highSamples = samples[highIndex]
 
 with open(outputFile,'w') as outFile:
      with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
          outFile.write(f'{minicondaBin}picard bcftools isec $WORK/SAEVA-outputs/bcfoutput/*.gz -p $WORK/SAEVA-outputs/All -n=length;\n')
-         outFile.write(f'{minicondaBin}picard bcftools isec $WORK/SAEVA-outputs/bcfoutput/*.gz -p $WORK/SAEVA-outputs/All -n=11;\n')
-         outFile.write(f'{minicondaBin}picard bcftools isec $WORK/SAEVA-outputs/bcfoutput/*.gz -p $WORK/SAEVA-outputs/All -n=11;\n')   
+         
+         outFile.write(f'{minicondaBin}picard bcftools isec $WORK/SAEVA-outputs/bcfoutput/*.gz -p $WORK/SAEVA-outputs/high -n=11;\n')
+         outFile.write(f'{minicondaBin}picard bcftools isec $WORK/SAEVA-outputs/bcfoutput/*.gz -p $WORK/SAEVA-outputs/low -n=11;\n')   
          outFile.write(f'sed -i 's/^chr/Chromosome/' $WORK/SAEVA-outputs/All/*.vcf;\n')
          outFile.write(f'sed -i 's/^chr/Chromosome/' $WORK/SAEVA-outputs/high/*.vcf;\n')
          outFile.write(f'sed -i 's/^chr/Chromosome/' $WORK/SAEVA-outputs/low/*.vcf;\n')
