cb36e453a135142cc828b32f63d2da6f543ce121
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-25 15:40:20 -0600

    Update pythonFreebayes.py

--- pythonFreebayes.py
+++ pythonFreebayes.py
@@ -8,7 +8,10 @@ inputFile = sys.argv[1]
 
 outputFile = "freebayes.sh"
 with open(outputFile,'w') as outFile:
+    count=0    
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'$WORK/SAEVA-softwares/freebayes/bin/freebayes -f $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/WholeGenomeFasta/genome.fa $WORK/SAEVA-outputs/picard/{row[0]}.picard.bam >$WORK/SAEVA-outputs/freebayesoutput/{row[0]}.vcf\n')
+            if count !=0:
+                outFile.write(f'$WORK/SAEVA-softwares/freebayes/bin/freebayes -f $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/WholeGenomeFasta/genome.fa $WORK/SAEVA-outputs/picard/{row[0]}.picard.bam >$WORK/SAEVA-outputs/freebayesoutput/{row[0]}.vcf\n')
+            count =count +1
