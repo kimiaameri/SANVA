12e2faadd27799fb50299adb3cddc2170f81e2a8
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-25 15:39:15 -0600

    Update pythonFinddepth.py

--- pythonFinddepth.py
+++ pythonFinddepth.py
@@ -9,9 +9,12 @@ inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
 outputFile = "findDepth.sh"
 with open(outputFile,'w') as outFile:
+    count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            outFile.write(f'{minicondaBin}samtools depth -a $WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.sam > $WORK/SAEVA-outputs/depth/{row[0]}.depth\n'}
+            if count !=0:
+                outFile.write(f'{minicondaBin}samtools depth -a $WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.sam > $WORK/SAEVA-outputs/depth/{row[0]}.depth\n'}
+            count =count + 1
 
  
