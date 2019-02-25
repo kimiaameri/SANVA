9d414399c8f031ff364b758fb66779e6c15166d0
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-25 15:31:22 -0600

    Update pythonTrimmomatic.py

--- pythonTrimmomatic.py
+++ pythonTrimmomatic.py
@@ -13,8 +13,8 @@ with open(outputFile,'w') as outFile:
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            if count =0
-               count +=
+            if count =0:
+                 count +=
             outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq \n')
             count +=
  
