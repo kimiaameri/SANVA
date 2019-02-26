c1892764f05e19e2e934f4268c4f45829055cc60
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-26 14:07:00 -0600

    Update pythonTrimmomatic.py

--- pythonTrimmomatic.py
+++ pythonTrimmomatic.py
@@ -1,12 +1,13 @@
 import csv
 import sys
 
-if len(sys.argv) < 3:
-    sys.stderr.write('No Input CSV file and miniconda path\n')
+if len(sys.argv) < 4:
+    sys.stderr.write('No Input CSV file and miniconda path GITHUB_PATH\n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
+githubPath = sys.argv[3]
 outputFile = "trimmomatic.sh"
 with open(outputFile,'w') as outFile:
     count=0
@@ -14,6 +15,6 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq ILLUMINACLIP:TruSeq3-PE.fa:3:30:10 SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:100\n')
+                outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/trimmomatic/{row[0]}-R1.paired.fq $WORK/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/trimmomatic/{row[0]}-R2.paired.fq $WORK/trimmomatic/{row[0]}-R2.unpaired.fq ILLUMINACLIP:{githubPath/}TruSeq3-PE.fa:3:30:10 SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:100\n')
             count =count + 1
  
