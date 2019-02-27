6b42aeec9eff5ef5028b0fdc09c4b3f964a540a2
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-02-27 17:28:30 -0600

    Update pythonBam.py

--- pythonBam.py
+++ pythonBam.py
@@ -1,8 +1,8 @@
 import csv
 import sys
 
-if len(sys.argv) < 3:
-    sys.stderr.write('No Input CSV file and miniconda path\n')
+if len(sys.argv) < 2:
+    sys.stderr.write('No Input CSV file \n')
     sys.exit(0)
     
 inputFile = sys.argv[1]
@@ -14,8 +14,8 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0 :
-               outFile.write(f'{minicondaBin}samtools view -bt $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SAEVA-outputs/samfiles/{row[0]}.sam >$WORK/SAEVA-outputs/bamfiles/{row[0]}.bam\n')
-               outFile.write(f'{minicondaBin}samtools flagstat $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam > $WORK/SAEVA-outputs/flagsam/{row[0]}.flagstat.log\n')
-               outFile.write(f'{minicondaBin}samtools sort $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam -O bam -o $WORK/SAEVA-outputs/flagsam/{row[0]}.sorted.bam\n')
+               outFile.write(f'$WORK/SAEVA-softwares/samtools-1.5/samtools view -bt $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SAEVA-outputs/samfiles/{row[0]}.sam >$WORK/SAEVA-outputs/bamfiles/{row[0]}.bam\n')
+               outFile.write(f'$WORK/SAEVA-softwares/samtools-1.5/samtools flagstat $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam > $WORK/SAEVA-outputs/flagsam/{row[0]}.flagstat.log\n')
+               outFile.write(f'$WORK/SAEVA-softwares/samtools-1.5/samtools sort $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam -O bam -o $WORK/SAEVA-outputs/flagsam/{row[0]}.sorted.bam\n')
             count =count +1
  
