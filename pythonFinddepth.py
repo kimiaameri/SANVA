200da89d6ada9925a8433babc15860e0a9ddd380
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-01 11:59:43 -0600

    Update pythonFinddepth.py

--- pythonFinddepth.py
+++ pythonFinddepth.py
@@ -6,7 +6,7 @@ if len(sys.argv) < 3:
     sys.exit(0)
     
 inputFile = sys.argv[1]
-SAMTools = sys.argv[2]
+SAMTools = sys.argv[2] 
 outputFile = "findDepth.sh"
 with open(outputFile,'w') as outFile:
     count=0
@@ -14,7 +14,5 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'{SAMTools}samtools depth -a $WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.sam > $WORK/SAEVA-outputs/depth/{row[0]}.depth\n'}
+                outFile.write(f'{SAMTools}samtools depth -a $WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.sam > $WORK/SAEVA-outputs/depth/{row[0]}.depth\n')
             count =count + 1
-
- 
