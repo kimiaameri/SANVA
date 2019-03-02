fb664e403819029731eb818fb417fd2c0a09d484
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-01 18:50:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ cd picard/
 wget https://github.com/broadinstitute/picard/releases/download/2.9.0/picard.jar
 
 ####   Download freebayes    #####
-cd $WORK/SAEVA-softwares/
+cd $WORK/SAEVA_softwares/freebayes/
 git clone --recursive git://github.com/ekg/freebayes.git
 cd freebayes
 make
