cc12e5e6be7362f5a697e609219ec596b871eb8a
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-04 14:28:02 -0600

    Create pythonIntersections.py

---
+++ pythonIntersections.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+
+if len(sys.argv) < 3:
+    sys.stderr.write('No Input CSV file and genomebed\n')
+    sys.exit(0)
+    
+inputFile = sys.argv[1]
+genomeBedpath = sys.argv[2]
+
+outputFile = "mapVCF-to-Bed.sh"
+with open(outputFile,'w') as outFile:
+    count=0
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if count !=0:
+                sed -i 's/^cpdna/chr/' {genomeBedpath}/nctc8325.bed;
+                outFile.write(f'$WORK/bedtools2/bin/bedtools intersect –a {row[0]}.vcf.bed -b > $WORK/SAEVA-outputs/vcfbed/{row[0]}.bed \n')
+            count =count + 1
+
