5202f993920e5085c671d199c084a08965638606
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-04 15:06:05 -0600

    Update significantGenes.sh

--- significantGenes.sh
+++ significantGenes.sh
@@ -1,7 +1,8 @@
 export GENOME_BED_PATH="$WORK/SAEVA_reference_genome/"
 export INTERSECTIONS_PATH="$WORK/SAEVA-outputs/intersections/"
+export OUTPUT_PATH='$WORK/SAEVA-outputs/'
 
 python3 pythonIntersections.py ../InputFiles.csv $GENOME_BED_PATH
 sh mapVCF-to-Bed.sh
 
-Rscript maincode.R GITHUB_DIR GENOME_BED_PATH INTERSECTIONS_PATH
+Rscript maincode.R GITHUB_DIR GENOME_BED_PATH INTERSECTIONS_PATH OUTPUT_PATH
