265222a1d6842814e3d17bbe8f2f6325aa553d0a
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-04 13:09:08 -0600

    Update depth.R

--- depth.R
+++ depth.R
@@ -1,14 +1,11 @@
 #-------------------- find quality distribuison --------------------#
 
 #-------------------- find depth distribuison --------------------#
-
-
 argv <- commandArgs(trailingOnly = TRUE)
 depthPath <- argv[1]
 vcfPath <- argv[2]
 depthOutput <- argv[3]
 qualityOutput <- argv[4]
-#summaryFile <- argv[5]
 
 depth.files<- list.files(depthPath, full.names=T)
 dist<-c()
@@ -16,12 +13,7 @@ for (f in depth.files){
   dep<-read.table(file=f,header=F,sep="\t",stringsAsFactors = F)
   dist<- c(dist,median(dep[,3]))
 }
-#means<- apply(dist,2,mean)
-#z<-as.data.frame(summary(dist))
-#minValue  <- min(dist)
 write(median(dist),depthOutput)
-#write.table(z,summaryFile)
-
 vcf.files <- list.files(vcfPath, full.names = T)
 qual <- c()
 for (f in vcf.files) {
