0a1194215cf355248bcc5a356de07f9bcfe8c7dd
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-04 13:35:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,12 @@ wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
 unzip snpEff_latest_core.zip
 rm snpEff_latest_core.zip
 
-
+####          bedtools        ####
+wget https://github.com/arq5x/bedtools2/releases/download/v2.25.0/bedtools-2.25.0.tar.gz
+tar -zxvf bedtools-2.25.0.tar.gz
+cd bedtools2
+make
 ###########################
+
 echo `$WORK/SAEVA-softwares/freebayes/bin/freebayes --version`
 
