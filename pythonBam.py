4e296372596b367fc1aa2e44b5982cea7d4eae58
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-04 17:16:23 -0600

    Update pythonBam.py

--- pythonBam.py
+++ pythonBam.py
@@ -17,7 +17,7 @@ with open(outputFile,'w') as outFile:
                outFile.write(f'{SAMTools}/samtools view -bt $WORK/SAEVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/BWAIndex/genome.fa $WORK/SAEVA-outputs/samfiles/{row[0]}.sam >$WORK/SAEVA-outputs/bamfiles/{row[0]}.bam\n')
                outFile.write(f'{SAMTools}/samtools flagstat $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam > $WORK/SAEVA-outputs/flagsam/{row[0]}.flagstat.log\n')
                outFile.write(f'{SAMTools}/samtools sort $WORK/SAEVA-outputs/bamfiles/{row[0]}.bam -O bam -o $WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.bam\n')
-               outFile.write(f'{SAMTools}/samtools stats $WORK/SAEVA-outputs/sortsam/{row[0]}.bam >$WORK/SAEVA-outputs/stats/{row[0]}.txt \n')
+               outFile.write(f'{SAMTools}/samtools stats $WORK/SAEVA-outputs/sortsam/{row[0]}.sorted.bam >$WORK/SAEVA-outputs/stats/{row[0]}.txt \n')
 
             count =count +1
  
