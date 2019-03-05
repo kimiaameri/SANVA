1fabfa41ba88fd9290dcf11ab48f58a4b715580d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-04 19:43:30 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cd SAEVA_reference_genome
 wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 tar -xzf Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 rm Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
-cd $WORK/SAEVA
+cd $WORK/SAEVA/
 python GenomeBedPull.py $WORK/SAEVA_reference_genome
 #######   Download softwares    #######
 mkdir SAEVA-softwares
@@ -61,7 +61,7 @@ cd $WORK/SAEVA-softwares/
 mkdir picard
 cd picard/
 wget https://github.com/broadinstitute/picard/releases/download/2.9.0/picard.jar
-
+cd ../
 ####   Download freebayes    #####
 cd $WORK/SAEVA-softwares/
 git clone --recursive git://github.com/ekg/freebayes.git
