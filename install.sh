49fed941c5f7ad80d6c506273c1f130897bc22e8
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-06 11:42:50 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ cd SAEVA_reference_genome
 wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 tar -xzf Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 rm Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
-cd $WORK/SAEVA/
-python GenomeBedPull.py $WORK/SAEVA_reference_genome
+
 #######   Download softwares    #######
 mkdir SAEVA-softwares
 cd SAEVA-softwares/
