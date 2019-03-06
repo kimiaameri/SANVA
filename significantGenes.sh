ad4685b5f9786b8ab2f606375a7e75bac739f482
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-06 12:01:06 -0600

    Update significantGenes.sh

--- significantGenes.sh
+++ significantGenes.sh
@@ -1,8 +1,18 @@
+cd $WORK/SAEVA-outputs/
+mkdir vcfbed
+mkdir intersection
+
+cd $WORK/SAEVA/
+python3 GenomeBedPull.py $WORK/SAEVA_reference_genome
 export GENOME_BED_PATH="$WORK/SAEVA_reference_genome/"
+python3 pythonVcfbed.py ../InputFiles.csv $MINICONDA_HOME
+sh vcfBed.sh
+python3 pythonIntersections.py ../InputFiles.csv $GENOME_BED_PATH
+sh mapVCF-to-Bed.sh
+
 export INTERSECTIONS_PATH="$WORK/SAEVA-outputs/intersections/"
 export OUTPUT_PATH='$WORK/SAEVA-outputs/'
 
-python3 pythonIntersections.py ../InputFiles.csv $GENOME_BED_PATH
 sh mapVCF-to-Bed.sh
 
 $WORK/SAEVA-outputs
