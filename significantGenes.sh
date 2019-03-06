ab8aa5d3e2c3388dc7e9f5aacd649d2112964927
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-06 14:51:58 -0600

    Update significantGenes.sh

--- significantGenes.sh
+++ significantGenes.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+#SBATCH --time=80:00:00   # Run time in hh:mm:ss
+#SBATCH --mem-per-cpu=16384     # Maximum memory required per CPU (in megabytes)
+#SBATCH --job-name=SAVEA
+#SBATCH --error=SAVEA.%J.err
+#SBATCH --output=SAVEA.%J.out
 cd $WORK/SAEVA-outputs/
 mkdir vcfbed
 mkdir intersection
