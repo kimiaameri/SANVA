524c8e4f345009d3d9017c4eeb3cacc84d4f0026
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-05 18:11:36 -0600

    Update depth.R

--- depth.R
+++ depth.R
@@ -9,16 +9,16 @@ qualityOutput <- argv[4]
 
 depth.files<- list.files(depthPath, full.names=T)
 dist<-c()
-for (f in depth.files){
-  dep<-read.table(file=f,header=F,sep="\t",stringsAsFactors = F)
+for (d in depth.files){
+  dep<-read.table(file=d,header=F,sep="\t",stringsAsFactors = F)
   dist<- c(dist,median(dep[,3]))
 }
 write(median(dist),depthOutput)
 #----------------------------------------------------------------------
 vcf.files <- list.files(vcfPath, full.names = T)
 qual <- c()
-for (f in vcf.files) {
-  vcf <- read.table(file=f)
+for (q in vcf.files) {
+  vcf <- read.table(file=q)
   qual <- c(qual,median(vcf[,6]))
 }
 quality<- as.integer(median(qual))
