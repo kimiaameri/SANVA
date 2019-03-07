421be10cb105dec94445d546857fe7d2d5396133
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-03-07 13:48:39 -0600

    Update significantGenes.sh

--- significantGenes.sh
+++ significantGenes.sh
@@ -4,20 +4,20 @@
 #SBATCH --job-name=SAVEA
 #SBATCH --error=SAVEA.%J.err
 #SBATCH --output=SAVEA.%J.out
-cd $WORK/SAEVA-outputs/
-mkdir vcfbed
-mkdir intersection
+#cd $WORK/SAEVA-outputs/
+#mkdir vcfbed
+#mkdir intersection
 
-cd $WORK/SAEVA/
-python3 GenomeBedPull.py $WORK/SAEVA_reference_genome 
+#cd $WORK/SAEVA/
+#python3 GenomeBedPull.py $WORK/SAEVA_reference_genome 
 export GENOME_BED_PATH="$WORK/SAEVA_reference_genome/"
-python3 pythonVcfbed.py ../InputFiles.csv $MINICONDA_HOME
-sh vcfBed.sh
-python3 pythonIntersections.py ../InputFiles.csv $GENOME_BED_PATH
-sh mapVCF-to-Bed.sh
+#python3 pythonVcfbed.py ../InputFiles.csv $MINICONDA_HOME
+#sh vcfBed.sh
+#python3 pythonIntersections.py ../InputFiles.csv $GENOME_BED_PATH
+#sh mapVCF-to-Bed.sh
 
 export INTERSECTIONS_PATH="$WORK/SAEVA-outputs/intersection/"
-export OUTPUT_PATH='$WORK/SAEVA-outputs'
+export OUTPUT_PATH="$WORK/SAEVA-outputs/"
 
 
 cd $WORK/SAEVA-outputs
@@ -27,7 +27,7 @@ export SOURCE_DIR="$WORK/SAEVA"
 
 cd $WORK/SAEVA_reference_genome
 cat  nctc8325.bed | tail -n+2 > nctc8325-1.bed 
-cd $WORK/SAEVA           
+cd $WORK/SAEVA/          
 Rscript maincode.R $SOURCE_DIR $GENOME_BED_PATH $INTERSECTIONS_PATH ../InputFiles.csv $HIGH $LOW bigtable.csv significatGenes.csv
 mv bigtable.csv $OUTPUT_PATH/
-mv significatGenes.csv $OUTPUT_PATH/
+mv significatGenes.csv $OUTPUT_PATH
