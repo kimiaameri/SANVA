e3dff36318f779d4d0c041423f058b77a13e3167
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-04-29 17:30:19 -0500

    Update SAEVA.sh

--- SAEVA.sh
+++ SAEVA.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time=80:00:00   # Run time in hh:mm:ss
-#SBATCH --mem-per-cpu=16384     # Maximum memory required per CPU (in megabytes)
+#SBATCH --mem-per-cpu=64gb     # Maximum memory required per CPU (in megabytes)
 #SBATCH --job-name=SAVEA
 #SBATCH --error=SAVEA.%J.err
 #SBATCH --output=SAVEA.%J.out
