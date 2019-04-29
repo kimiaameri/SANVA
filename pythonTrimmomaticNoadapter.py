369f2247284c960307058c73ab678caad73dfef2
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-04-29 18:36:42 -0500

    Update and rename pythonTrimmomatic.py to pythonTrimmomaticNoadapter.py

---
+++ pythonTrimmomaticNoadapter.py
@@ -0,0 +1,20 @@
+import csv
+import sys
+
+if len(sys.argv) < 4:
+    sys.stderr.write('No Input CSV file and miniconda path GITHUB_PATH\n')
+    sys.exit(0)
+    
+inputFile = sys.argv[1]
+minicondaBin = sys.argv[2]
+githubPath = sys.argv[3]
+outputFile = "trimmomatic.sh"
+with open(outputFile,'w') as outFile:
+    count=0
+    with open(inputFile) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if count !=0:
+                outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R2.paired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R2.unpaired.fq SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:100\n')
+            count =count + 1
+ 
