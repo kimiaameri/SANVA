b52cf802f12c53c826baf7a8f3cfc2a4da81b748
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-05-01 15:19:35 -0500

    Update depth.R

--- depth.R
+++ depth.R
@@ -10,16 +10,20 @@ qualityOutput <- argv[4]
 depth.files<- list.files(depthPath, full.names=T)
 dist<-c()
 for (d in depth.files){
+  if(file.info(d)$size > 0) {
   dep<-read.table(file=d,header=F,sep="\t",stringsAsFactors = F)
   dist<- c(dist,median(dep[,3]))
+    }
 }
 write(min(dist),depthOutput)
 #----------------------------------------------------------------------
 vcf.files <- list.files(vcfPath, full.names = T)
 qual <- c()
 for (q in vcf.files) {
+  if(file.info(q)$size > 20) {
   vcf <- read.table(file=q)
   qual <- c(qual,median(vcf[,6]))
+    }
 }
 quality<- as.integer(min(qual))
 write(quality,qualityOutput)
