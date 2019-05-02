695737b7c634956a049df8aef32ea221655de962
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-05-02 13:32:42 -0500

    Update intesect_reference_vcf.R

--- intesect_reference_vcf.R
+++ intesect_reference_vcf.R
@@ -3,6 +3,7 @@ argv <- commandArgs(trailingOnly = TRUE)
 
 intesect_reference_vcf <- function(i, reference,intersectionspath){
    intersections <- as.matrix(read.table(paste0(intersectionspath,i,sep=""),header=F,sep="\t",stringsAsFactors = F))
+   if(file.info(intersections)$size > 0) {
     length.intersection= nrow(intersections)
     length.genome= nrow(reference)
     for (j in 1 :length.genome)
@@ -11,6 +12,6 @@ intesect_reference_vcf <- function(i, reference,intersectionspath){
           if (intersections[i,2] >= reference[j,2] & intersections[i,2] <= reference[j,3]) 
               intersections[i,4] = reference [j,4]
     }
-   
+   }
   return(table(intersections[,4]))
 }
