f10f7c1b793b890b1ec61ba562fed8dbb4208084
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-05-08 03:13:07 -0500

    Update pythonSnpEffMerge.py

--- pythonSnpEffMerge.py
+++ pythonSnpEffMerge.py
@@ -19,15 +19,15 @@ with open(inputFile) as csv_file:
         if count !=0 :
           all.append(row[0])
         count =count +1
-    print(all)
-
     for sample in all :
         all.append(prefix + sample + ".vcf.gz")
         
-        allStr  = ' '.join(all)   
-        print(allStr)
+    allStr  = ' '.join(all) 
+    with open (cpath +"/all.txt",'w') as of:
+         of.write(str(all))
+    length=len(all)
 with open(outputFile,'w') as outFile:
-        outFile.write(f'{BCFTools}bcftools merge --force {allStr} -O v -o $WORK/SAEVA-outputs/merge -n=100;\n')
+        outFile.write(f'{BCFTools}bcftools merge --force {allStr} -O v -o $WORK/SAEVA-outputs/merge -n={length};\n')
         outFile.write('sed -i \'s/^chr/Chromosome/\' $WORK/SAEVA-outputs/merge.vcf;\n')
         outFile.write(f'{minicondaBin}java -Xmx4g -jar $WORK/SAEVA-softwares/snpEff/snpEff.jar -v Staphylococcus_aureus_subsp_aureus_nctc_8325 $WORK/SAEVA-outputs/merg.vcf > $WORK/SAEVA-outputs/snpEff.ann.vcf \n')
         outFile.write('mv $WORK/ACMBCB/snpEff_genes.txt $WORK/SAEVA-outputs/snpEff.txt \n')
