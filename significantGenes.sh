7cfcbd501f5492c745e2a932e534b344d0dec7fe
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-05-29 18:11:21 -0500

    Update significantGenes.sh

--- significantGenes.sh
+++ significantGenes.sh
@@ -29,4 +29,10 @@ cd $WORK/SANVA/
 Rscript maincode.R $SOURCE_DIR $GENOME_BED_PATH $INTERSECTIONS_PATH ./InputFiles.csv bigtable.csv tableWeight.csv 
 mv bigtable.csv $OUTPUT_PATH/
 
-Python3 stringMapping.py Genes.txt
+Rscript String-Prepration.R $SOURCE_DIR $OUTPUT_PATH/bigtable.csv $OUTPUT_PATH/GenesList.csv $OUTPUT_PATH/GenesList.txt
+
+Python3 stringMapping.py $OUTPUT_PATH/GenesList.txt $OUTPUT_PATH/string_mapping.tsv
+Python3 STRINGNetworkInteractions.py $OUTPUT_PATH/GenesList.txt $OUTPUT_PATH/string_interactions.tsv
+
+Rscript String-Prepration.R $OUTPUT_PATH/bigtable.csv $OUTPUT_PATH/string_interactions.tsv $OUTPUT_PATH/string_mapping.tsv $OUTPUT_PATH/PPI.txt $OUTPUT_PATH/mutational.txt $OUTPUT_PATH/weights.txt             
+
