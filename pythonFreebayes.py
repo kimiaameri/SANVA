41001fc787595c6b721584b151f8aea3a12ebb7b
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-12 17:27:47 -0500

    Update pythonFreebayes.py

--- pythonFreebayes.py
+++ pythonFreebayes.py
@@ -5,7 +5,7 @@ if len(sys.argv) < 3:
     sys.exit(0)
     
 inputFile = sys.argv[1]
-freebayes = sys.argv[2]
+minicondaBin = sys.argv[2]
 
 outputFile = "freebayes.sh"
 with open(outputFile,'w') as outFile:
@@ -14,5 +14,5 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'{freebayes}freebayes -f $WORK/SANVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/WholeGenomeFasta/genome.fa $WORK/SANVA-outputs/picard/{row[0]}.picard.bam >$WORK/SANVA-outputs/freebayesoutput/{row[0]}.vcf\n')
+                outFile.write(f'{minicondaBin}freebayes -f $WORK/SANVA_reference_genome/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Sequence/WholeGenomeFasta/genome.fa $WORK/SANVA-outputs/picard/{row[0]}.picard.bam >$WORK/SANVA-outputs/freebayesoutput/{row[0]}.vcf\n')
             count =count +1
