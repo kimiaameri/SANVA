4affcdc706f6d3f4e2080ebbcd964da31fa3d5aa
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-12 18:17:56 -0500

    Update pythonBCF_VCF.py

--- pythonBCF_VCF.py
+++ pythonBCF_VCF.py
@@ -16,8 +16,8 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                outFile.write(f'$WORK/SANVA-softwares/freebayes/vcflib/bin/vcffilter -f "QUAL >{quality}" $WORK/SANVA-outputs/freebayesoutput/{row[0]}.vcf >$WORK/SANVA-outputs/vcffilter-q/{row[0]}.vcf\n')
-                outFile.write(f'$WORK/SANVA-softwares/freebayes/vcflib/bin/vcffilter -f "DP > {depth}" $WORK/SANVA-outputs/vcffilter-q/{row[0]}.vcf > $WORK/SANVA-outputs/vcffilter-q-dp/{row[0]}.vcf\n')
+                outFile.write(f'{minicondaBin}vcffilter -f "QUAL >{quality}" $WORK/SANVA-outputs/freebayesoutput/{row[0]}.vcf >$WORK/SANVA-outputs/vcffilter-q/{row[0]}.vcf\n')
+                outFile.write(f'{minicondaBin}vcffilter -f "DP > {depth}" $WORK/SANVA-outputs/vcffilter-q/{row[0]}.vcf > $WORK/SANVA-outputs/vcffilter-q-dp/{row[0]}.vcf\n')
                 outFile.write(f'{minicondaBin}bcftools view -Ob $WORK/SANVA-outputs/vcffilter-q-dp/{row[0]}.vcf > $WORK/SANVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')NN
                 outFile.write(f'{minicondaBin}bcftools index $WORK/SANVA-outputs/bcfoutput/{row[0]}.vcf.gz\n')N
             count =count + 1
