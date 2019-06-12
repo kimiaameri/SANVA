99a03e2e6ec317b4a1c685ca93cf638877b3866d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-12 17:26:59 -0500

    Update pythonPicard.py

--- pythonPicard.py
+++ pythonPicard.py
@@ -5,7 +5,7 @@ if len(sys.argv) < 3:
     sys.exit(0)
     
 inputFile = sys.argv[1]
-picard = sys.argv[2]
+minicondaBin = sys.argv[2]
 
 outputFile = "picard.sh"
 with open(outputFile,'w') as outFile:
@@ -14,5 +14,5 @@ with open(outputFile,'w') as outFile:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:
-                 outFile.write(f'java -jar {picard}picard.jar MarkDuplicates I=$WORK/SANVA-outputs/sortsam/{row[0]}.sorted.bam O=$WORK/SANVA-outputs/picard/{row[0]}.picard.bam M=$WORK/SANVA-outputs/picard/picardlog/{row[0]}.picard.log\n')
+                 outFile.write(f'java -jar {minicondaBin}picard.jar MarkDuplicates I=$WORK/SANVA-outputs/sortsam/{row[0]}.sorted.bam O=$WORK/SANVA-outputs/picard/{row[0]}.picard.bam M=$WORK/SANVA-outputs/picard/picardlog/{row[0]}.picard.log\n')
             count=count+1
