4dd5f72ed2ace5b836ff9cf3987e1af976b664a4
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-13 14:24:55 -0500

    Update pythonIntersections.py

--- pythonIntersections.py
+++ pythonIntersections.py
@@ -11,12 +11,12 @@ minicondaBin = sys.argv[3]
 
 outputFile = "mapVCF-to-Bed.sh"
 with open(outputFile,'w') as outFile:
-    outFile.write(f'sed -i \'s/^cpdna/chr/\' {genomeBedpath}/nctc8325.bed;\n')  
+    outFile.write(f'sed -i \'s/^cpdna/chr/\' {genomeBedpath}nctc8325.bed;\n')  
     count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if count !=0:                          
-                outFile.write(f'{minicondaBin}bedtools intersect -a $WORK/SAEVA-outputs/vcfbed/{row[0]}.bed -b {genomeBedpath}nctc8325.bed > $WORK/SAEVA-outputs/intersection/{row[0]}.bed \n')
+                outFile.write(f'{minicondaBin}bedtools intersect -a $WORK/SANVA-outputs/vcfbed/{row[0]}.bed -b {genomeBedpath}nctc8325.bed > $WORK/SANVA-outputs/intersection/{row[0]}.bed \n')
             count =count + 1
 
