edd0de6472a806b509faf81982a7d1b0789a429e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-13 02:40:50 -0500

    Update installTools.sh

--- installTools.sh
+++ installTools.sh
@@ -9,7 +9,7 @@ cd SANVA-softwares/
 ####      Bioconda          ####
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
-cd 
+cd $WORK/SANVA
 conda env create -f sanva.environment.yaml
 conda activate
 conda activate sanva
