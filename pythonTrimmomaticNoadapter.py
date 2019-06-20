7198dc6ceda3802f6db3e59d710d5c379bf82e30
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-20 14:52:00 -0500

    Update pythonTrimmomaticNoadapter.py

--- pythonTrimmomaticNoadapter.py
+++ pythonTrimmomaticNoadapter.py
@@ -10,12 +10,12 @@ minicondaBin = sys.argv[2]
 githubPath = sys.argv[3]
 outputFile = "trimmomatic.sh"
 with open(outputFile,'w') as outFile:
-   # count=0
+    count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-          #  if count !=0:
+            if count !=0:
                outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SANVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog $WORK/SANVA/SANVA-data/{row[0]}_1.fastq $WORK/SANVA/SANVA-data/{row[0]}_2.fastq $WORK/SANVA-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SANVA-outputs/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/SANVA-outputs/trimmomatic/{row[0]}-R2.paired.fq $WORK/SANVA-outputs/trimmomatic/{row[0]}-R2.unpaired.fq SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:100\n')
                #outFile.write(f'{minicondaBin}trimmomatic PE -threads 4 -phred33 -trimlog $WORK/SAEVA-outputs/trimmomatic/trimlog/{row[0]}.trimlog {row[2]} {row[3]} $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R1.paired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R1.unpaired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R2.paired.fq $WORK/SAEVA-outputs/trimmomatic/{row[0]}-R2.unpaired.fq SLIDINGWINDOW:4:15 MAXINFO:50:0.5 LEADING:3 TRAILING:3 MINLEN:100\n')
-           # count =count + 1
+            count =count + 1
  
