9132800a98a41f06bf6a533cd3c597319cf6df6e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-06-28 17:41:23 -0500

    Update maincode.R

--- maincode.R
+++ maincode.R
@@ -27,10 +27,11 @@ colnames(bigtable) <- intersections
 bigtable.norm<-bigtable
 for (i in 1:length(intersections))
 {
+  if(names(res) != ".") {
   res <- intesect_reference_vcf(intersections[i],reference_Genome,intersectionspath)
   bigtable[names(res),i] <-  as.numeric(res) 
   bigtable.norm[names(res),i] <-  as.numeric(res) / as.numeric(reference_Genome[i,10])
-}
+}}
 bigtable <- bigtable[rowSums(bigtable)!= 0,]
 bigtable.norm <- bigtable.norm[rowSums(bigtable.norm)!= 0,]
 #-------------------------------------------------------------------------#
