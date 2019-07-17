284f917258d8660865aa558c46d14c69ab736e86
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-17 16:05:52 -0500

    Update pythonBam.py

--- pythonBam.py
+++ pythonBam.py
@@ -9,6 +9,13 @@ inputFile = sys.argv[1]
 minicondaBin = sys.argv[2]
 outputFile = "bam.sh"
 with open(outputFile,'w') as outFile:
+    outFile.write('#!/bin/sh \n')
+    outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
+    outFile.write('#SBATCH --mem-per-cpu=64gb  \n')
+    # Maximum memory required per CPU (in megabytes')
+    outFile.write('#SBATCH --job-name=BAM \n')
+    outFile.write('#SBATCH --error=BAM.%J.err \n')
+    outFile.write('#SBATCH --output=BAM.%J.out \n')  
     count=0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
