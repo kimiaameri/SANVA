40b5d289e4f6f3dec909da8a5d70b43786db4568
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2019-07-17 16:08:56 -0500

    Update pythonPicard.py

--- pythonPicard.py
+++ pythonPicard.py
@@ -9,6 +9,13 @@ minicondaBin = sys.argv[2]
 
 outputFile = "picard.sh"
 with open(outputFile,'w') as outFile:
+    outFile.write('#!/bin/sh \n')
+    outFile.write('#SBATCH --time=100:00:00   # Run time in hh:mm:ss  \n')
+    outFile.write('#SBATCH --mem-per-cpu=64gb  \n')
+    # Maximum memory required per CPU (in megabytes')
+    outFile.write('#SBATCH --job-Picard=Trim \n')
+    outFile.write('#SBATCH --error=Picard.%J.err \n')
+    outFile.write('#SBATCH --output=Picard.%J.out \n')  
     count = 0
     with open(inputFile) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
